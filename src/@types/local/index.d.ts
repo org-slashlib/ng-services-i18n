dbcf5bf814cc5896dacf21a05cb66a403e6c4d46
db-developer <db-developer@gmx.de>
2019-05-05 19:38:43 +0200

    extended interface Navigator Signed-off-by: db-developer <db-developer@gmx.de>

--- src/@types/local/index.d.ts
+++ src/@types/local/index.d.ts
@@ -4,3 +4,12 @@
 interface String {
   format( ...replacements: string[] ):string;
 }
+
+// @see https://zzz.buzz/2016/01/13/detect-browser-language-in-javascript/
+interface Navigator {
+  browserLanguage?: string;
+  // language:      string;                 // => interface NavigatorLanguage
+  // languages:     ReadonlyArray<string>;  // => interface NavigatorLanguage
+  systemLanguage?:  string;
+  userLanguage?:    string;
+}
